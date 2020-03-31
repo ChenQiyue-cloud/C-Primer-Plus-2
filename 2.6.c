b1c2e6756f3606acbb2480c4bbd80606a643b735
ChenQiyue-cloud <55774342+ChenQiyue-cloud@users.noreply.github.com>
2020-03-31 11:38:25 +0800

    Update 2.6.c

--- 2.6.c
+++ 2.6.c
@@ -5,7 +5,7 @@ int main()
 {
     const int toes = 10;
     printf("toes = %d\n",toes);
-    printf("toes * 2 = %d\n",toes*toes);
+    printf("toes * 2 = %d\n",toes*2);
     printf("toes ^ 2 = %d\n",toes*toes);
 
     return 0;
