9faabf24a23ae7a2ae38f1e254d1fa12ab38aec5
ChenQiyue-cloud <55774342+ChenQiyue-cloud@users.noreply.github.com>
2020-03-31 11:36:28 +0800

    Create 2.6.c

---
+++ 2.6.c
@@ -0,0 +1,12 @@
+/*6.编写一个程序，创建一个整型变量toes,并将toes设置为10.程序中还要
+计算toes的两倍和toes的平方。该程序应打印3个值，并分别描述以示区分。*/
+#include<stdio.h>
+int main()
+{
+    const int toes = 10;
+    printf("toes = %d\n",toes);
+    printf("toes ^ 2 = %d\n",toes*toes);
+    printf("toes ^ 3 = %d\n",toes*toes*toes);
+
+    return 0;
+}
