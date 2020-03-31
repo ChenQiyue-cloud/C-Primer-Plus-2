81e3934963b55a3fd90db2ca12b862376231c076
ChenQiyue-cloud <55774342+ChenQiyue-cloud@users.noreply.github.com>
2020-03-31 11:11:52 +0800

    Update 2.2.c

--- 2.2.c
+++ 2.2.c
@@ -1,8 +1,8 @@
 /*编写一个程序，打印你的姓名和地址*/
-include<stdio.h>
+#include<stdio.h>
 int main()
 {
-printf("Name:Chen\n");
+    printf("Name:Chen\n");
     printf("Addr:ShenZhen, Guangdong province\n");
 
     return 0;
